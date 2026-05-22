83ba58cc9476b466ca8c81c47c3bae4959e7e6d0
Abhishek-k1 <abhishek97766@gmail.com>
2026-05-22 16:21:54 +0530

    Basics improvement

--- clubhub.cpp
+++ clubhub.cpp
@@ -1,45 +1,46 @@
-#include <iostream>
-#include <fstream>
+#include <iostream>    // For cout, cin
+#include <fstream>    // For file handling
 
 using namespace std;
 
-class Club
+class Club      // class for storing club details
 {
-    public:
-      string clubName;
-      string category;
-      string coordinator;       
+public:
+  string clubName;   // stores club name
+  string category;   // stores club category
+  string coordinator;  // stores coordinator name 
 };
 
 class Student
 {
-  public:
-      string studentName;
-      string usn;
-      string branch;
-      string interestedclub;
+public:
+  string studentName;   // student name
+  string usn;           // student USN
+  string branch;        // student branch
+  string interestedclub;   // Interseted club
 };
 
-class Event
+class Event      // class for storing event details
 {
-  public:
-      string eventName;
-      string venue;
-      string timing;
-      string clubName;
+public:
+  string eventName;  // Event name
+  string venue;      // Event venue
+  string timing;     // Event timing
+  string clubName;   // Related club name
 };
-      
+
 int main()
 {
-  Club c;
-  Student s;
-  string searchClub;
-  Event e;
-  int choice;
+  Club c;             // Object of Club class
+  Student s;          // object of student class
+  string searchClub;  // variable for searching club
+  Event e;            // Object  of Event class
+  int choice;         // Stoes menu choice
 
-  while(true)
+  while (true)
   {
-    cout << "\n===== CLUBHUB MENU =====\n";
+    // Main menu display
+    cout << "\n============= WELCOME TO CLUBHUB SYSTEM ==============\n";
     cout << "1. Add Club\n";
     cout << "2. Display Club\n";
     cout << "3. Register Student\n";
@@ -49,21 +50,23 @@ int main()
     cout << "7. Display Events\n";
     cout << "8. Admin Menu\n";
     cout << "9. Exit\n";
-    
 
     cout << "Enter Choice: ";
-    cin >> choice;
+    cin >> choice;      // user enters menu choice
 
-    if(choice == 1)
+    // ADD CLUB FEATURE
+    if (choice == 1)
     {
       cout << "Enter Club Name: ";
-      cin >> c.clubName;
+      cin.ignore();                  // Clears newline from buffer
+      getline(cin, c.clubName);      // Takes full line input
 
       cout << "Enter Club Category: ";
       cin >> c.category;
 
-      cout  << "Enter Coordinator Name: ";
-      cin >> c.coordinator;
+      cout << "Enter Coordinator Name: ";
+      cin.ignore();
+      getline(cin, c.coordinator);
 
       ofstream file("clubs.txt", ios::app);
 
@@ -71,62 +74,74 @@ int main()
       file << c.category << " ";
       file << c.coordinator << endl;
 
-      file.close();
-
-
+      file.close();    // Close file
 
       cout << "Club Added Successfully!\n";
+    }
 
-
-     }
-
+    // Display CLUB FEATURE
     else if (choice == 2)
     {
-      ifstream file("clubs.txt");
+      ifstream file("clubs.txt");    // Opens file for reading
 
+    // Checks if file exists
+      if (!file)
+      {
+        cout << "No Club Data Found!\n";
+      }
       string name, category, coordinator;
 
-      while(file >> name >> category >> coordinator)
+    // Reads data from file
+      while (file >> name >> category >> coordinator)
       {
 
+        cout << "\n==================================\n";
         cout << "\nClub Name: " << name << endl;
         cout << "Category: " << category << endl;
         cout << "Coordinator: " << coordinator << endl;
+        cout << "===================================\n";
+      }
+      file.close();
     }
-    file.close();
-  }
 
+  // STUDENT REGISTRATION
     else if (choice == 3)
     {
-      cout <<  "Enter Student Name: ";
-      cin >> s.studentName;
+      cout << "Enter Student Name: ";
+      cin.ignore();
+      getline(cin, s.studentName);
 
       cout << "Enter USN: ";
       cin >> s.usn;
 
-      cout  << "Enter Branch: ";
-      cin >> s.branch;
+      cout << "Enter Branch: ";
+      cin.ignore();
+      getline(cin, s.branch);
 
       cout << "Enter Interested Club: ";
-      cin >> s.interestedclub;
+      getline(cin, s.interestedclub);
 
       cout << "Student Registered Successfully!\n";
     }
 
-    else if(choice == 4)
+    // DISPLAY STUDENT DETAILS
+    else if (choice == 4)
     {
-      cout  << "\nStudent Name: " << s.studentName << endl;
+      cout << "\nStudent Name: " << s.studentName << endl;
       cout << "USN: " << s.usn << endl;
       cout << "Branch: " << s.branch << endl;
       cout << "Interested Club: " << s.interestedclub << endl;
     }
 
-    else if(choice == 5)
+    // SEARCH CLUB FEATURE
+    else if (choice == 5)
     {
       cout << "Enter club Name to Search: ";
-      cin >> searchClub;
+      cin.ignore();
+      getline(cin, searchClub);
 
-      if(searchClub == c.clubName)
+    // Compares searched club with storeed club
+      if (searchClub == c.clubName)
       {
         cout << "\nClub Found!\n";
 
@@ -139,51 +154,57 @@ int main()
         cout << "Club Not Found!\n";
       }
     }
-      else if(choice == 6)
-      {
-        cout << "Enter Event Name: ";
-        cin >> e.eventName;
 
-        cout << "Enter Venue: ";
-        cin >> e.venue;
+    // ADD EVENT FEATURE
+    else if (choice == 6)
+    {
+      cout << "Enter Event Name: ";
+      cin.ignore();
+      getline(cin, e.eventName);
 
-        cout << "Enter Timing: ";
-        cin >> e.timing;
+      cout << "Enter Venue: ";
+      getline(cin, e.venue);
 
-        cout << "Enter Club Name: ";
-        cin >> e.clubName;
+      cout << "Enter Timing: ";
+      cin >> e.timing;
 
-        ofstream file("events.txt", ios::app);
+      cout << "Enter Club Name: ";
+      cin.ignore();
+      getline(cin, e.clubName);
 
-        file << e.eventName << " ";
-        file << e.venue << " ";
-        file << e.timing << " ";
-        file << e.clubName << endl;
+      ofstream file("events.txt", ios::app);
 
-        file.close();
+      file << e.eventName << " ";
+      file << e.venue << " ";
+      file << e.timing << " ";
+      file << e.clubName << endl;
 
-        cout << "Event Added Successfully!\n";
-      }
+      file.close();
 
-      else if(choice == 7)
-      {
-        ifstream file("events.txt");
-
-        string eventName, venue, timing, clubName;
-
-        while(file >> eventName >> venue >> timing >> clubName)
-        {
-          cout << "\nEvent Name: " << eventName << endl;
-          cout << "Venue: " << venue << endl;
-          cout << "Timing: " << timing << endl;
-          cout << "club Name: " << clubName << endl;
-        }
-
-        file.close();
-      
+      cout << "Event Added Successfully!\n";
     }
 
-    else if(choice == 8)
+    // DISPLAY EVENTS
+    else if (choice == 7)
+    {
+      ifstream file("events.txt");
+
+      string eventName, venue, timing, clubName;
+
+    // Reads event details from file
+      while (file >> eventName >> venue >> timing >> clubName)
+      {
+        cout << "\nEvent Name: " << eventName << endl;
+        cout << "Venue: " << venue << endl;
+        cout << "Timing: " << timing << endl;
+        cout << "Club Name: " << clubName << endl;
+      }
+
+      file.close();
+    }
+
+    // ADMIN MENU
+    else if (choice == 8)
     {
       int adminChoice;
 
@@ -196,46 +217,56 @@ int main()
       cout << "Enter Choice: ";
       cin >> adminChoice;
 
-      if(adminChoice == 1)
+      // VIEW CLUBS
+      if (adminChoice == 1)
       {
         ifstream file("clubs.txt");
 
         string name, category, coordinator;
 
-        while(file >> name >> category >> coordinator)
+        while (file >> name >> category >> coordinator)
         {
           cout << "\nClub Name: " << name << endl;
-          cout << "Category: " << category <<  endl;
+          cout << "Category: " << category << endl;
           cout << "Coordinator: " << coordinator << endl;
         }
 
         file.close();
       }
-        else if(adminChoice == 2)
-        {
-          cout << "\nStudent Name: " << s.studentName << endl;
-          cout << "USN: " << s.usn << endl;
-          cout << "Branch: " << s.branch << endl;
-          cout << "Interested Club: " << s.interestedclub << endl;
-        }
-        else if(adminChoice == 3)
-        {
-          cout << "Remove Club Feature Coming Soon!\n";
-        }
-        else if(adminChoice == 4)
-        {
-          cout <<  "Returning to Main Menu...\n";
-        }
+
+      // VIEW STUDENTS
+      else if (adminChoice == 2)
+      {
+        cout << "\nStudent Name: " << s.studentName << endl;
+        cout << "USN: " << s.usn << endl;
+        cout << "Branch: " << s.branch << endl;
+        cout << "Interested Club: " << s.interestedclub << endl;
+      }
+
+      // REMOVE CLUB PLACEHOLDER
+      else if (adminChoice == 3)
+      {
+        cout << "Remove Club Feature Coming Soon!\n";
+      }
+
+      // RETURN TO MAIN MENU
+      else if (adminChoice == 4)
+      {
+        cout << "Returning to Main Menu...\n";
+      }
     }
-    else if  (choice == 9)
+
+    // EXIT PROGRAM
+    else if (choice == 9)
     {
-       cout << "Exiting Program...";
-        break;
+      cout << "\nThank You For Using ClubHub System!\n";
+      break;
     }
+
+    // INVALID CHOICE
     else
     {
-        cout << "Invalid Choice";
+      cout << "Invalid Choice! Please Try Again.\n";
     }
   }
 }
-
